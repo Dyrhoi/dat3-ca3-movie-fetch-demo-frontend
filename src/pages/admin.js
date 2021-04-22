17f56d12b1dd9ee479c7dac397c681e10d595152
worldjones <worldjones@gmail.com>
2021-04-22 12:17:46 +0200

    fix

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,17 +1,24 @@
-import { apiUrl } from '../settings'
+import facade from "../authenticationFacade"
 import React, { useState, useEffect } from "react"
+import {apiUrl} from "../settings"
 
-const URL = apiUrl + "/ext/admin"
+const URL = apiUrl
 
 function Admin() {
     const [content, setContent] = useState([])
+    const fetchData = () => {
+        const options = facade.makeOptions("GET", true); //True add's the token
+        return fetch(URL + "api/info/admin", options).then(facade.handleHttpErrors);
+      };
     useEffect(() => {
-        fetch(URL).then((res) => res.json()).then((data) => {
-            setContent(data.results)
-        });
+       setContent (fetchData())
     }, [])
     return (
-        content
+        <div>
+
+   {JSON.stringify(content)}
+
+        </div>
     )
 }
 
